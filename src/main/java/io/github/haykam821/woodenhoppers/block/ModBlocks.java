9f991846b879257d90149291e38d6a9508800ec3
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-11-26 13:41:00 -0500

    Add bamboo hoppers

--- src/main/java/io/github/haykam821/woodenhoppers/block/ModBlocks.java
+++ src/main/java/io/github/haykam821/woodenhoppers/block/ModBlocks.java
@@ -3,8 +3,8 @@ package io.github.haykam821.woodenhoppers.block;
 import io.github.haykam821.woodenhoppers.Main;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
@@ -12,19 +12,21 @@ import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.resource.featuretoggle.FeatureFlag;
+import net.minecraft.resource.featuretoggle.FeatureFlags;
 import net.minecraft.util.Identifier;
 
 public enum ModBlocks implements ItemConvertible {
-	OAK_HOPPER("oak_hopper", Material.WOOD, Blocks.OAK_PLANKS),
-	SPRUCE_HOPPER("spruce_hopper", Material.WOOD, Blocks.SPRUCE_PLANKS),
-	BIRCH_HOPPER("birch_hopper", Material.WOOD, Blocks.BIRCH_PLANKS),
-	JUNGLE_HOPPER("jungle_hopper", Material.WOOD, Blocks.JUNGLE_PLANKS),
-	ACACIA_HOPPER("acacia_hopper", Material.WOOD, Blocks.ACACIA_PLANKS),
-	DARK_OAK_HOPPER("dark_oak_hopper", Material.WOOD, Blocks.DARK_OAK_PLANKS),
-	MANGROVE_HOPPER("mangrove_hopper", Material.WOOD, Blocks.MANGROVE_PLANKS),
-	CRIMSON_HOPPER("crimson_hopper", Material.NETHER_WOOD, Blocks.CRIMSON_PLANKS),
-	WARPED_HOPPER("warped_hopper", Material.NETHER_WOOD, Blocks.WARPED_PLANKS);
+	OAK_HOPPER("oak_hopper", Blocks.OAK_PLANKS),
+	SPRUCE_HOPPER("spruce_hopper", Blocks.SPRUCE_PLANKS),
+	BIRCH_HOPPER("birch_hopper", Blocks.BIRCH_PLANKS),
+	JUNGLE_HOPPER("jungle_hopper", Blocks.JUNGLE_PLANKS),
+	ACACIA_HOPPER("acacia_hopper", Blocks.ACACIA_PLANKS),
+	DARK_OAK_HOPPER("dark_oak_hopper", Blocks.DARK_OAK_PLANKS),
+	MANGROVE_HOPPER("mangrove_hopper", Blocks.MANGROVE_PLANKS),
+	BAMBOO_HOPPER("bamboo_hopper", Blocks.BAMBOO_PLANKS, FeatureFlags.UPDATE_1_20),
+	CRIMSON_HOPPER("crimson_hopper", Blocks.CRIMSON_PLANKS),
+	WARPED_HOPPER("warped_hopper", Blocks.WARPED_PLANKS);
 
 	private final Block base;
 	private final Block block;
@@ -42,8 +44,8 @@ public enum ModBlocks implements ItemConvertible {
 		Registry.register(Registries.ITEM, id, this.item);
 	}
 
-	private ModBlocks(String path, Material material, Block base) {
-		this(path, base, new WoodenHopperBlock(Block.Settings.of(material, base.getDefaultMapColor()).strength(2).sounds(BlockSoundGroup.WOOD).nonOpaque()));
+	private ModBlocks(String path, Block base, FeatureFlag... features) {
+		this(path, base, new WoodenHopperBlock(createBlockSettings(base, features)));
 	}
 
 	public Block getBase() {
@@ -63,5 +65,15 @@ public enum ModBlocks implements ItemConvertible {
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register(entries -> {
 			entries.addAfter(Items.HOPPER, ModBlocks.values());
 		});
+	}
+
+	private static Block.Settings createBlockSettings(Block base, FeatureFlag... features) {
+		BlockState baseState = base.getDefaultState();
+
+		return Block.Settings.of(baseState.getMaterial(), base.getDefaultMapColor())
+			.strength(2)
+			.sounds(baseState.getSoundGroup())
+			.nonOpaque()
+			.requires(features);
 	}
 }
